a080e376060c4365092809bb626b73b01767e6b1
RenatoCostaDev <renatotrybe@gmail.com>
2020-10-18 13:08:54 -0300

    Comitando app e css inicial

---
+++ redux-simple-math/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';import './App.css';
+import Card from './Card';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Math React-Redux (Simple)</h1>
+      <div className="linha">
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default App;
